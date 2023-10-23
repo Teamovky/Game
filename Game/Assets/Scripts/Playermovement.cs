9b8cd3bb6bd73993befce2208bfc2e78b6d7a7a2
MichaelReeves2 <tadeas.jindra28@seznam.cz>
2023-10-23 18:37:21 +0200

    FPV drone throttle and smooth tilt addition

--- Game/Assets/Scripts/Playermovement.cs
+++ Game/Assets/Scripts/Playermovement.cs
@@ -2,39 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//Movement of FPV drone
 public class Playermovement : MonoBehaviour
 {
     //natáčení po x, z o 7.5 stupně
-    public float speed = 0.1f;
+
+    Rigidbody player;
     private float right_left_Input;
     private float forward_backward_Input;
     private bool fall_Input;
     private Vector2 tilt;
-
     private Vector2 turn; //.x = x // .y = z
     public float mouse_sens = 5f;
     private bool jump_Input;
-
-    float timeCount = 0.0f;
+    private float timeCount = 0.0f;
+    public float speed = .25f;
+    public float thrust = 0f;
 
     public GameObject cam1;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GetComponent<Rigidbody>();
         Cursor.lockState = CursorLockMode.Locked;
+        //m_Rigidbody.AddForce(transform.up * m_Thrust);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (cam1.activeSelf) {
+        
+        if (cam1.activeSelf) 
+        {
             right_left_Input = Input.GetAxis("Horizontal");
             forward_backward_Input = Input.GetAxis("Vertical");
             fall_Input = Input.GetKey(KeyCode.LeftShift);
             jump_Input = Input.GetKey(KeyCode.Space);
-            Debug.Log("R+L " + right_left_Input + " F+B " + forward_backward_Input + " Tilt Y " + tilt.x + " Tilt X " + tilt.y);
+            //Debug.Log("R+L " + right_left_Input + " F+B " + forward_backward_Input + " Tilt Y " + tilt.x + " Tilt X " + tilt.y);
+
             if(right_left_Input > 0)
             {
                 tilt.x = -7.5f;
@@ -60,22 +65,31 @@ public class Playermovement : MonoBehaviour
             {
                 tilt.y = 0f;
             }
-            transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(tilt.y,turn.x,tilt.x),(timeCount*0.01f));
+
+            transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(tilt.y,turn.x,tilt.x),(timeCount*speed));
             transform.Translate(Vector3.right * timeCount * speed * right_left_Input);
             transform.Translate(Vector3.forward * timeCount * speed * -forward_backward_Input);
-            Debug.Log(timeCount);
-            if(fall_Input)
+            //Debug.Log(timeCount);
+
+            if(fall_Input && thrust >= -10)
             {
-                transform.Translate(Vector3.up * timeCount * -speed);
+                thrust -= 5f * Time.deltaTime;
             }
 
             turn.x += (Input.GetAxis("Mouse X") * mouse_sens);
-            transform.localRotation = Quaternion.Euler(0, turn.x,0);
+            // transform.localRotation = Quaternion.Euler(0, turn.x,0);
     
-            if(jump_Input)
+            if(jump_Input && thrust <= 10)
             {
-                transform.Translate(Vector3.up * timeCount * speed);
+                thrust += 5f * Time.deltaTime;
             }
+
+            if(thrust < -1 || thrust > 1)
+            {
+                player.AddForce(Vector3.up * thrust);
+            }
+
+            //Debug.Log(thrust);
 
             timeCount = timeCount + Time.deltaTime;
         }
